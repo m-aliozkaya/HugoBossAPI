603a5abf072699ce5194ae5fd721c6da4c630144
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-12-15 20:13:12 +0300

    some bugs fixed

--- DataAccess/Abstract/IFactoryOrderDal.cs
+++ DataAccess/Abstract/IFactoryOrderDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IFactoryOrderDal : IEntityRepository<Season>
+    public interface IFactoryOrderDal : IEntityRepository<FactoryOrder>
     {
     }
 }
